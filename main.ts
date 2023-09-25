d9a6311a929684778c71d59016e66a8c59dbe984
selimgayretli <113355046+selimgayretli@users.noreply.github.com>
2023-09-25 19:45:25 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
-makerbit.onIrDatagram(function () {
-    if (makerbit.irButton() == makerbit.irButtonCode(IrButton.Number_1)) {
-        basic.showIcon(IconNames.Yes)
-    } else {
+OLED.init(128, 64)
+let strip = neopixel.create(DigitalPin.P8, 3, NeoPixelMode.RGB)
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P13) == 1) {
         basic.showIcon(IconNames.No)
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        OLED.writeStringNewLine("Motion Detected!")
+        music.play(music.stringPlayable("B - B - B - B - ", 120), music.PlaybackMode.UntilDone)
+    } else {
+        basic.showIcon(IconNames.Happy)
+        OLED.clear()
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
     }
 })
-makerbit.connectIrReceiver(DigitalPin.P0, IrProtocol.NEC)
